98a675215d0d137a821fdcb1d18f5b32f55a2560
Awilixsh <awilixsh@gmail.com>
2024-09-30 15:29:59 +0800

    Added Navigation Bar for Mobile and Desktop

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function test(){
-	console.log('test');
+function openMenu(){
+	const buttonHamburger = document.getElementById('imgBurger-mobile');
+	const bgOverlay = document.getElementById('bgOverlay');
+	const navBar = document.getElementById('navMain');
+	buttonHamburger.style.display = 'none';
+	bgOverlay.style.display = 'block';
+	navBar.style.display = 'grid';
+}
+
+function closeMenu(){
+	const buttonHamburger = document.getElementById('imgBurger-mobile');
+	const bgOverlay = document.getElementById('bgOverlay');
+	const navBar = document.getElementById('navMain');
+	buttonHamburger.style.display = 'block';
+	bgOverlay.style.display = 'none';
+	navBar.style.display = 'none';
 }
